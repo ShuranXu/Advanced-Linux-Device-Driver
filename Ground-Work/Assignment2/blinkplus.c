354525b6880984d1d67f40d2eb4924759b52f666
ShuranXu <shuran.xu@mail.utoronto.ca>
2021-08-11 14:35:37 -0400

    Added skeleton code for ioctl for blinkplus driver and completed __init and __exit functions

--- Ground-Work/Assignment2/blinkplus.c
+++ Ground-Work/Assignment2/blinkplus.c
@@ -39,6 +39,25 @@ blinkplus_t blinkplus;
 #define ALL_LEDS_ON   0x07
 #define RESTORE_LEDS  0xFF
 
+/* This is to create /dev/blinkplus device nodes */
+
+static char mydev_name[]="blinkplus";  // This will appears in /proc/devices
+static struct cdev  *blink_cdev;
+static struct class *blink_class;
+static dev_t  dev;
+
+static int blink_open(struct inode *inode, struct file *file);
+static ssize_t blink_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+static int blink_release(struct inode *inode, struct file *file);
+
+static struct file_operations blink_fops = {
+	open: blink_open,
+	release: blink_release,
+	unlocked_ioctl: blink_ioctl,
+	owner:	 THIS_MODULE
+};
+
+
 /*
  * Function my_timer_func blinks the keyboard LEDs periodically by invoking
  * command KDSETLED of ioctl() on the keyboard driver. To learn more on virtual 
@@ -55,8 +74,8 @@ blinkplus_t blinkplus;
  */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
-static void my_timer_func(unsigned long ptr)
-{
+static void my_timer_func(unsigned long ptr){
+
 	int *pstatus = (int *)ptr;
 
 	if (*pstatus == ALL_LEDS_ON)
@@ -77,13 +96,12 @@ static void my_timer_func(unsigned long ptr)
 	(my_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED, *pstatus);
 
 	my_timer.expires = jiffies + BLINK_DELAY;
-        printk("my_timer_func, %d", *pstatus);
+    printk("my_timer_func, %d", *pstatus);
 	add_timer(&my_timer);
 }
 #else 
-static void my_timer_func(struct timer_list * data)
-{
-	// container_of(h, struct sensor_private_data, early_suspend);   
+static void my_timer_func(struct timer_list * data){
+
 	blinkplus_t *blinkplusptr = container_of(data, blinkplus_t, my_timer);
 	int *pstatus = (int*)blinkplusptr->kbledstatus;
 
@@ -101,9 +119,27 @@ static void my_timer_func(struct timer_list * data)
 }
 #endif 
 
-static int __init kbleds_init(void)
-{
+static ssize_t blink_ioctl(struct file *file, unsigned int cmd, unsigned long arg){
+
+	printk("blinkplus: ioctl\n");
+	return 0;
+}
+
+static int blink_open(struct inode *inode, struct file *file){
+	printk("blinkplus: open\n");
+	return 0;
+}
+
+static int blink_release(struct inode *inode, struct file *file){
+	printk("blinkplus: release\n");
+	return 0;
+}
+
+static int __init kbleds_init(void){
+
 	int i;
+	int result;
+	int major;
 
 	printk(KERN_INFO "kbleds: loading\n");
 	printk(KERN_INFO "kbleds: fgconsole is %x\n", fg_console);
@@ -132,11 +168,50 @@ static int __init kbleds_init(void)
 	timer_setup(&blinkplus.my_timer, my_timer_func, 0); 
 	#endif 
 
+	result = alloc_chrdev_region(&dev, 0, 1, mydev_name);
+	if (result<0) 
+		return result;
+
+    major = MAJOR(dev);
+
+	printk("The device is registered by Major no: %d\n", major);
+
+	// Allocate a cdev structure 
+    blink_cdev = cdev_alloc();
+	
+	// Attach hello fops methods with the cdev: blink_cdev->ops=&blink_fops 
+	cdev_init (blink_cdev, &blink_fops);
+    blink_cdev->owner = THIS_MODULE;
+
+	// Connect the assigned major number to the cdev 
+    result = cdev_add(blink_cdev, dev, 1);
+    if (result<0){
+		printk("Error in registering the module\n");
+        unregister_chrdev_region(dev, 1);
+        return result;
+    }
+
+	printk(KERN_INFO "blinkplus: %d\n",__LINE__);
+	// Create a device class 
+	blink_class = class_create(THIS_MODULE,mydev_name);
+	if (IS_ERR(blink_class)) {
+		printk(KERN_ERR "Error creating hello class.\n");
+		result = PTR_ERR(blink_class);
+		cdev_del(blink_cdev);
+		unregister_chrdev_region(dev, 1);
+		return -1;
+    }
+	// Create and add device under blink_class
+    device_create(blink_class,NULL,dev,NULL,"blinkplus%d",0);
+
+	printk(KERN_INFO "blinkplus: %d\n",__LINE__);
+	printk(KERN_INFO "blinkplus: loading\n");
+
 	return 0;
 }
 
-static void __exit kbleds_cleanup(void)
-{
+static void __exit kbleds_cleanup(void){
+
 	printk(KERN_INFO "kbleds: unloading...\n");
 	#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 	del_timer(&my_timer);
@@ -144,6 +219,11 @@ static void __exit kbleds_cleanup(void)
 	del_timer(&blinkplus.my_timer);
 	#endif 
 	(my_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED, RESTORE_LEDS);
+
+	cdev_del(blink_cdev);
+	device_destroy(blink_class, dev);
+	class_destroy(blink_class);
+	unregister_chrdev_region(dev,1);
 }
 
 module_init(kbleds_init);
--- Ground-Work/Assignment2/blinkplus.mod.c
+++ Ground-Work/Assignment2/blinkplus.mod.c
@@ -26,18 +26,28 @@ MODULE_INFO(retpoline, "Y");
 static const struct modversion_info ____versions[]
 __used __section("__versions") = {
 	{ 0x243c757b, "module_layout" },
+	{ 0xcb91c2e0, "class_destroy" },
+	{ 0x19e0af64, "device_destroy" },
 	{ 0x2ea2c95c, "__x86_indirect_thunk_rax" },
 	{ 0x2b68bd2f, "del_timer" },
+	{ 0x27d03f38, "device_create" },
+	{ 0x19794500, "cdev_del" },
+	{ 0x47fa1b1c, "__class_create" },
+	{ 0x6091b333, "unregister_chrdev_region" },
+	{ 0x5ffccd73, "cdev_add" },
+	{ 0x807ede20, "cdev_init" },
+	{ 0x23ae549f, "cdev_alloc" },
+	{ 0xe3ec2f2b, "alloc_chrdev_region" },
 	{ 0xc6f46339, "init_timer_key" },
 	{ 0xd102a031, "vc_cons" },
 	{ 0x4e6e8ea7, "fg_console" },
 	{ 0x24d273d1, "add_timer" },
-	{ 0xc5850110, "printk" },
 	{ 0x15ba50a6, "jiffies" },
+	{ 0xc5850110, "printk" },
 	{ 0xbdfb6dbb, "__fentry__" },
 };
 
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "AD8EE0E14F2AE20C7724998");
+MODULE_INFO(srcversion, "7A7D7D56A710A2BF0E50CA9");
