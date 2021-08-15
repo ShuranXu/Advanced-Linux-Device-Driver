31f1343052e4acc3d38065826dace1b1cd0435a2
ShuranXu <shuran.xu@mail.utoronto.ca>
2021-08-15 14:53:47 -0400

    Modified startvm.sh and made the network work

--- PCI_and_PCIE/PCI-PROJECT/startvm.sh
+++ PCI_and_PCIE/PCI-PROJECT/startvm.sh
@@ -13,24 +13,43 @@ if [ `id -u` -ne 0 ]; then
     err_exit "This script must be run as root"
 fi
 
+
+INTERFACE="enp0s31f6"
+USER="shuran"
+
 # Setup networking
 
 if ! ifconfig tap0 > /dev/null 2>&1; then
-echo "Setting up networking"
-tunctl || err_exit "Error creating tap0"
-ip addr flush dev tap0 || err_exit "ip addr flush"
-ip addr add 192.168.1.1/26 dev tap0 || err_exit "ip addr add"
-ip link set dev tap0 up || err_exit "ip link set"
-/bin/sh -c 'echo 1 > /proc/sys/net/ipv4/ip_forward' \
-|| err_exit "ip_forward"
+    echo "Setting up networking"
+    ip link add br0 type bridge
+    ip link set br0 up
 
-iptables -t nat -A POSTROUTING -s 192.168.1.0/26 -j MASQUERADE \
-|| err_exit "iptables"
-dnsmasq -F 192.168.1.2,192.168.1.2 -a 192.168.1.1 -z \
-|| err_exit "dnsmasq"
-fi
+    ip link set "$INTERFACE" master br0
+
+    # Drop existing IP from INTERFACE
+    ip addr flush dev "$INTERFACE"
+
+    # Assign IP to br0
+    ip addr add 192.168.2.10/24 brd + dev br0
+    ip route add default via 192.168.2.1 dev br0
+
+    # setup tap0 interface
+    ip tuntap add dev tap0 mode tap user "$USER"
+    ip link set dev tap0 up
+    ip link set tap0 master br0
+fi 
 
 # Start the guest
-qemu-system-x86_64 -enable-kvm -m 4096 -hda ubuntu_14_04.img -vnc :1 \
--netdev tap,id=e1000,ifname=tap0,script=no -device virtio-net,netdev=e1000 \
--netdev user,id=rtl8139 -device rtl8139,netdev=rtl8139
+
+echo "Setting up VM"
+
+qemu-system-x86_64 \
+-enable-kvm \
+-m 4096 \
+-hda ubuntu_14_04.img \
+-vnc :1 \
+-netdev tap,id=e1000,ifname=tap0,script=no \
+-device virtio-net,netdev=e1000 \
+-netdev user,id=rtl8139 \
+-device rtl8139,netdev=rtl8139
+
