3528cadffc0789ab10473974ae4715b68ae72db7
Shuran Xu <shuran.xu@mail.utoronto.ca>
2021-08-17 16:36:11 -0400

    Built pci_template.ko with prink lines

--- PCI_and_PCIE/PCI-PROJECT/Phase-I/pci_template.mod.c
+++ PCI_and_PCIE/PCI-PROJECT/Phase-I/pci_template.mod.c
@@ -20,16 +20,16 @@ __attribute__((section("__versions"))) = {
 	{ 0xc6c01fa, __VMLINUX_SYMBOL_STR(module_layout) },
 	{ 0x7d0571e5, __VMLINUX_SYMBOL_STR(pci_unregister_driver) },
 	{ 0x4203c482, __VMLINUX_SYMBOL_STR(__pci_register_driver) },
-	{ 0xbbd78bd4, __VMLINUX_SYMBOL_STR(dma_ops) },
-	{ 0xf15e6c6b, __VMLINUX_SYMBOL_STR(dev_err) },
 	{ 0x62e4100, __VMLINUX_SYMBOL_STR(register_netdev) },
 	{ 0xf10de535, __VMLINUX_SYMBOL_STR(ioread8) },
 	{ 0x363cd1b7, __VMLINUX_SYMBOL_STR(alloc_etherdev_mqs) },
 	{ 0x2b9e8aac, __VMLINUX_SYMBOL_STR(dma_supported) },
+	{ 0xbbd78bd4, __VMLINUX_SYMBOL_STR(dma_ops) },
 	{ 0x42c8de35, __VMLINUX_SYMBOL_STR(ioremap_nocache) },
 	{ 0x1fedf0f4, __VMLINUX_SYMBOL_STR(__request_region) },
 	{ 0x69a358a6, __VMLINUX_SYMBOL_STR(iomem_resource) },
 	{ 0x48254905, __VMLINUX_SYMBOL_STR(pci_set_master) },
+	{ 0xf15e6c6b, __VMLINUX_SYMBOL_STR(dev_err) },
 	{ 0xb19de74, __VMLINUX_SYMBOL_STR(pci_enable_device) },
 	{ 0x81fcd7c8, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
 	{ 0x92a94ad2, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
@@ -51,4 +51,4 @@ __attribute__((section(".modinfo"))) =
 
 MODULE_ALIAS("pci:v000010ECd00008139sv*sd*bc*sc*i*");
 
-MODULE_INFO(srcversion, "67515263B1EAB2765CA5721");
+MODULE_INFO(srcversion, "C01073AF44E5E9E76D14A81");
